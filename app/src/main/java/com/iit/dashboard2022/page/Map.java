9cd8a32f5f9e6a8fc94025e65fbbbf4f0090d58d
Eric Li <eli7@hawk.iit.edu>
2022-02-21 21:47:55 -0600

    added initial map page code

--- app/src/main/java/com/iit/dashboard2022/page/Map.java
+++ app/src/main/java/com/iit/dashboard2022/page/Map.java
@@ -1,10 +1,15 @@
 package com.iit.dashboard2022.page;
 
+import static com.iit.dashboard2022.ecu.ECUMsgHandler.STATE.Button;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,10 +24,24 @@ public class Map extends Page {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        String TAG = "Logging";
         TextView coordinate_number;
         int numbers = 0; // placeholder for receiving data
 
         View view = (ViewGroup) inflater.inflate(R.layout.map_layout, container, false);
+
+        // Start new activity to show map
+        Button map_btn = view.findViewById(R.id.show_map);
+        map_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent logDetailIntent = new Intent(getContext().getApplicationContext(), PolyActivity.class);
+                startActivity(logDetailIntent);
+                Log.i(TAG, "Displaying Map");
+            }
+        });
+
+
         // On receiving data, update textview
         TextView data = (TextView) getView().findViewById(R.id.coordinate_number);
         data.setText(numbers);
